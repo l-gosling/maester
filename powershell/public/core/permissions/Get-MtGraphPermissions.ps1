33bede3ae3e13ff2038c7a01264baf198d867cb7
Lukas Gosling <71852244+l-gosling@users.noreply.github.com>
2025-09-05 18:30:31 +0200

    Add script to get general data from mgContext

---
+++ powershell/public/core/permissions/Get-MtGraphPermissions.ps1
@@ -0,0 +1,30 @@
+﻿
+function Get-MtGraphPermissions {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param()
+
+    try {
+        # Get Microsoft Graph context informations
+        $context = Get-MgContext
+
+        # Get additional informations from context data
+        $__MtSession.Permissions.AuthType = $context.AuthType
+        Write-Verbose "Authtype is '$($__MtSession.Permissions.AuthType)'"
+
+        $__MtSession.Permissions.ApplicationId = $context.ClientId
+        Write-Verbose "ApplicationId is '$($__MtSession.Permissions.ApplicationId)'"
+
+        if ($context.AuthType -eq 'Delegated') {
+            $__MtSession.Permissions.AccountName = $context.Account
+        } elseif ($context.AuthType -eq 'AppOnly' -or $context.AuthType -eq 'ManagedIdentity' ) {
+            $__MtSession.Permissions.AccountName = $context.AppName
+            $__MtSession.Permissions.AccountId = $context.ClientId
+        }
+        return
+    }
+    catch {
+        Write-Verbose "Error getting Graph permissions: $($_.Exception.Message)"
+        throw $_
+    }
+}
