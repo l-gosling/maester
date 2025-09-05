6568f6468fccdaa504b8367363d81f6407c1985b
Lukas Gosling <71852244+l-gosling@users.noreply.github.com>
2025-09-05 19:21:51 +0200

    Add Get-MtEntraRoleActions function to retrieve actions from user role assignments

---
+++ powershell/public/core/permissions/Get-MtEntraRoleActions.ps1
@@ -0,0 +1,33 @@
+﻿
+function Get-MtEntraRoleActions {
+    [CmdletBinding()]
+    [OutputType([PSCustomObject])]
+    param()
+
+    try {
+
+        # Get role assignments for current user that are not limited by administrativ units
+        $userRoleAssignments = Invoke-MtGraphRequest -RelativeUri "roleManagement/directory/roleAssignments" -Filter "principalId eq '$($__MtSession.Permissions.AccountId)' and directoryScopeId eq '/'"
+
+        # Get role definitions for assigned roles only
+        $assignedRoleIds = $userRoleAssignments.RoleDefinitionId
+        $roleDefinitions = @()
+        foreach ($roleId in $assignedRoleIds) {
+            $roleDefinition = Invoke-MtGraphRequest -RelativeUri "roleManagement/directory/roleDefinitions/$roleId"
+                $roleDefinitions += $roleDefinition.rolePermissions.allowedResourceActions
+        }
+
+        # Remove duplicates from roleDefinitions array
+        $roleDefinitions = $roleDefinitions | Select-Object -Unique
+
+        # Add permissions to script variable
+        $__MtSession.Permissions.EntraAction = $roleDefinitions
+        Write-Verbose "Entra role actions count is '$(($__MtSession.Permissions.EntraAction).Count)'"
+
+        return
+    }
+    catch {
+        Write-Verbose "Error getting Graph permissions: $($_.Exception.Message)"
+        throw $_
+    }
+}
